2f2411ca9b60c1bf6fc6d2da8a32b6d6aa071920
Alvaro Serero <alvaroserero@gmail.com>
2026-09-08 17:13:22 +0200

    Gradient with PyTorch Autograd

---
+++ pytorch/gradient_with_pytorch_autograd.py
@@ -0,0 +1,14 @@
+"""Compute a Gradient with PyTorch Autograd"""
+
+import torch
+
+def grad_of_quadratic(x_value: float) -> float:
+    # TODO: build a tracked leaf for x, compute f(x), run backprop, return df/dx as a float
+    x = torch.tensor(x_value, requires_grad = True)
+    y = x ** 2 + 3 * x + 2
+    y.backward()
+    return x.grad.item()
+
+
+print(grad_of_quadratic(2.0))
+print(grad_of_quadratic(0.0))
